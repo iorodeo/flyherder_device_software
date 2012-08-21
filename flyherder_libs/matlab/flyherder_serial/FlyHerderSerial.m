0437b97df283c8edb89a515401f165ae08b41daf
Will Dickson <will@iorodeo.com>
2012-08-20 17:03:44 -0700

    Worked on dynamically generated methods for matlab communications class.

--- flyherder_libs/matlab/flyherder_serial/FlyHerderSerial.m
+++ flyherder_libs/matlab/flyherder_serial/FlyHerderSerial.m
@@ -1,5 +1,56 @@
-classdef FlyHerderSerial < handle 
+%
+% FlyHerderSerial  - provides a serial interface to the fly herder hardware.
+%
+% Normal methods
+% --------------
+% 
+% Dynamically generated Methods
+% -----------------------------
+%   getDevInfo
+%   getCmds
+%   getRspCodes
+%   getNumAxis
+%   getNumDim
+%   getAxisNames
+%   getDimNames
+%   getAxisOrder
+%   getDimOrder
+%   getAllowedOrientation
+%   setDrivePowerOn
+%   setDrivePowerOff
+%   isDrivePowerOn
+%   stop
+%   isRunning
+%   enable
+%   disable
+%   isEnabled
+%   moveToPosition
+%   moveAxisToPosition
+%   moveToHome
+%   isInHomePosition
+%   setMaxSeparation
+%   getMaxSeparation
+%   getPosition
+%   getAxisPosition
+%   setMaxSpeed
+%   getMaxSpeed
+%   setAcceleration
+%   getAcceleration
+%   setOrientation
+%   getOrientation
+%   setAxisOrientation
+%   getAxisOrientation
+%   setStepsPerMM
+%   getStepsPerMM
+%   setSerialNumber
+%   getSerialNumber
+%   getModelNumber
+%
+% Author: Will Dickson, IO Rodeo Inc.
+% 
 
+classdef FlyHerderSerial < handle 
+    
     properties
         dev = [];
     end
@@ -12,7 +63,7 @@ classdef FlyHerderSerial < handle
 
     properties (Constant) 
 
-        % serial communication parameters
+        % Serial communication parameters.
         baudrate = 9600;
         databits = 8;
         stopbits = 1;
@@ -21,7 +72,7 @@ classdef FlyHerderSerial < handle
         resetDelay = 2.0;
         inputBufferSize = 2048;
 
-        % command ids for basic commands
+        % Command ids for basic commands.
         cmdIdGetDevInfo = 0;
         cmdIdGetCmds = 1;
         cmdIdGetRspCodes = 2;
@@ -38,6 +89,7 @@ classdef FlyHerderSerial < handle
     methods 
 
         function obj = FlyHerderSerial(port)
+        % FlyHerderSerial - class constructor.
             obj.dev = serial( ...
             port, ...
             'baudrate', obj.baudrate, ...
@@ -51,6 +103,7 @@ classdef FlyHerderSerial < handle
         end
 
         function open(obj)
+        % Open - opens a serial connection to the fly herder hardware.
             if obj.isOpen == false
                 fopen(obj.dev);
                 pause(obj.resetDelay);
@@ -61,12 +114,14 @@ classdef FlyHerderSerial < handle
         end
 
         function close(obj)
+        % Close - closes the connection to the device.
             if obj.isOpen == true
                 fclose(obj.dev);
             end
         end
 
         function delete(obj)
+        % Delete - deletes the object.
             if obj.isOpen
                 obj.close();
             end
@@ -74,6 +129,8 @@ classdef FlyHerderSerial < handle
         end
 
         function isOpen = get.isOpen(obj)
+        % get.isOpen - returns true/false depending on whether the connection
+        % to the device is open.
             status = get(obj.dev,'Status');
             if strcmpi(status,'open')
                 isOpen = true;
@@ -82,16 +139,30 @@ classdef FlyHerderSerial < handle
             end
         end
 
+        function printDynamicMethods(obj)
+        % printDyamicMethods - prints all dynamically generated class methods.
+            cmdIdNames = fieldnames(obj.cmdIdStruct);
+            fprintf('\n');
+            fprintf('FlyHerder Dynamic Methods\n');
+            fprintf('-------------------------\n');
+            for i = 1:length(cmdIdNames)
+                fprintf('%s\n',cmdIdNames{i});
+            end
+        end
+
         function devInfo = get.devInfo(obj)
+        % get.devInfo - returns the device information structure.
             devInfo = obj.devInfoStruct;
         end
 
 
         function cmdIds = get.cmdIds(obj)
+        % get.cmdIds - returns the sturcture of command Ids.
             cmdIds = obj.cmdIdStruct;
         end
 
         function rspCodes = get.rspCodes(obj)
+        % get.rspCodes - returns the structure of device response codes.
             rspCodes = obj.rspCodeStruct;
         end
 
@@ -99,17 +170,24 @@ classdef FlyHerderSerial < handle
         %methods (Access=private)
 
         function rspStruct = sendCmd(obj,cmdId,varargin)
+        % sendCmd - sends a command to device and reads the device's response.
+        % The device responds to all commands with a serialized json string.
+        % This string is parsed into a Matlab structure.
+        %
+        % Arguments:
+        %  cmdId    = the Id number of the command
+        %  varagin  = any additional arguments required by the command
             if obj.isOpen
                 % Send command to device
                 cmdStr = obj.createCmdStr(cmdId, varargin);
-                fprintf(obj.dev,'%s\n',cmdStr);
+                fprintf(obj.dev,'%c\n',cmdStr);
 
                 % Get response as json string and parse
-                rspStrJson = fscanf(obj.dev,'%s',1000);
+                rspStrJson = fscanf(obj.dev,'%c');
                 try
                     rspStruct = loadjson(rspStrJson);
                 catch ME
-                    disp('unable to parse device response');
+                    disp('Unable to parse device response');
                     rethrow(ME);
                 end
 
@@ -118,12 +196,12 @@ classdef FlyHerderSerial < handle
                     rspCmdId = rspStruct.cmdId;
                     rspStruct = rmfield(rspStruct, 'cmdId');
                 catch ME
-                    disp('device response does not contain command Id')
+                    disp('Device response does not contain command Id')
                     rethrow(ME);
                 end
                 if rspCmdId ~= cmdId
                     errMsg = sprintf( ...
-                        'command Id returned, %d, does not match that sent, %d', ...
+                        'Command Id returned, %d, does not match that sent, %d', ...
                         rspCmdId, ...
                         cmdId ...
                         );
@@ -135,7 +213,7 @@ classdef FlyHerderSerial < handle
                     rspStatus = rspStruct.status;
                     rspStruct = rmfield(rspStruct,'status');
                 catch ME
-                    disp('device response does not contain status');
+                    disp('Device response does not contain status');
                     rethrow(ME);
                 end
 
@@ -146,59 +224,61 @@ classdef FlyHerderSerial < handle
                             errMsg = rspStruct.errMsg;
                         catch ME
                             errMsg = 'repsonse status ~= rspSuccess &' 
-                            errMsg = [errMsg, 'device response does not contain errMsg'];
+                            errMsg = [errMsg, ' device response does not contain errMsg'];
                         end
-                        error(errMsg)
+                        error(errMsg);
                     end
                 end
 
             else
-                error('connection must be open to send message');
+                error('Connection must be open to send message');
             end
         end
 
-        function cmdStr = createCmdStr(obj, cmdId, cmdArgCellArray) 
+        function cmdStr = createCmdStr(obj, cmdId, cmdArgs) 
+        % createCmdStr - create a command string for sending to the device given
+        % the cmdId number and a cell array of the commands arguments.
             cmdStr = sprintf('[%d',uint16(cmdId));
-            for cmdArg = cmdArgCellArray 
-                cmdArg = cmdArg{1}
-                switch class(cmdArg)
+            for i=1:length(cmdArgs)
+                arg = cmdArgs{i};
+                switch class(arg)
                     case 'double'
-                        cmdStr = sprintf('%s, %f', cmdStr, cmdArg);
+                        cmdStr = sprintf('%s, %f', cmdStr, arg);
                     case 'char'
-                        cmdStr = sprintf('%s, %s', cmdStr, cmdArg);
+                        cmdStr = sprintf('%s, %s', cmdStr, arg);
                     otherwise
-                        error('unknown type %s',class(cmdArg));
+                        error('unknown type %s',class(arg));
                 end
             end
             cmdStr = sprintf('%s]',cmdStr);
         end
 
-        function createCmdIdStruct(obj)
-            % Get structure of command Ids from device.
-            obj.cmdIdStruct = obj.sendCmd(obj.cmdIdGetCmds);
-        end
-
-        function createRspCodeStruct(obj)
-            % Get structure of response codes from the device.
-            obj.rspCodeStruct = obj.sendCmd(obj.cmdIdGetRspCodes);
-        end
-
-        function createDevInfoStruct(obj)
-            % Get device information struture from device
-            obj.devInfoStruct = obj.sendCmd(obj.cmdIdGetDevInfo);
-        end
-
-        function subsref(obj,S)
-            % Overload subsref in order to creaet dynamic methods from cmdIdStruct.
+        function varargout = subsref(obj,S)
+        % subsref - overloaded subsref function to enable dynamic generation of 
+        % class methods from the cmdIdStruct structure. 
+            val = [];
             if obj.isDynamicMethod(S)
-                disp('dynamic method')
-                S(1).subs
+                val = obj.dynamicMethodFcn(S);
             else
-                builtin('subsref',obj,S);
+                if nargout == 0
+                    builtin('subsref',obj,S);
+                else
+                    val = builtin('subsref',obj,S);
+                end
+            end
+            if ~isempty(val)
+                varargout = {val};
             end
         end
 
         function flag = isDynamicMethod(obj,S)
+        % isDynamicMethod - used in the subsred function to determine whether
+        % or not the method is dynamically generated. This is determined by
+        % whether or not the name of the method given method is also the name
+        % of a field in the cmdIdStruct. 
+        %
+        % Arguments:
+        %  S = 'type' + 'subs' stucture passed to subsref function
             flag = false;
             if ~isempty(obj.cmdIdStruct)
                 if S(1).type == '.' & isfield(obj.cmdIdStruct,S(1).subs)
@@ -207,6 +287,36 @@ classdef FlyHerderSerial < handle
             end
         end
 
+        function rtnVal = dynamicMethodFcn(obj,S)
+        % dynamicMethodFcn - implements a the dynamically generated methods.
+            cmdName = S(1).subs;
+            cmdArgs = S(2).subs;
+            cmdId = obj.cmdIdStruct.(cmdName);
+            rspStruct = obj.sendCmd(cmdId,cmdArgs{:});
+            rspFieldNames = fieldnames(rspStruct);
+            if length(rspFieldNames) == 0
+                rtnVal = [];
+            elseif length(rspFieldNames) == 1;
+                rtnVal = rspStruct.(rspFieldNames{1});
+            else
+                rtnVal = rspStruct;
+            end
+        end
+
+        function createCmdIdStruct(obj)
+        % createCmdIdStruct - gets structure of command Ids from device.
+            obj.cmdIdStruct = obj.sendCmd(obj.cmdIdGetCmds);
+        end
+
+        function createRspCodeStruct(obj)
+        % createRspCodeStruct - gets structure of response codes from the device.
+            obj.rspCodeStruct = obj.sendCmd(obj.cmdIdGetRspCodes);
+        end
+
+        function createDevInfoStruct(obj)
+        % createDevInfoStruct - get device information struture from device.
+            obj.devInfoStruct = obj.sendCmd(obj.cmdIdGetDevInfo);
+        end
 
     end
     
--- flyherder_libs/matlab/flyherder_serial/test_flyherder.m
+++ flyherder_libs/matlab/flyherder_serial/test_flyherder.m
@@ -1,11 +1,7 @@
 function test_flyherder
     x = FlyHerderSerial('com67');
     x.open();
-    x.getDevInfo()
-    %x.rspCodes
-    %x.cmdIds
-    %x.devInfo
-    %%rsp = x.sendCmd(1);
-    %x.close();
+    x.getAxisPosition('y0')
+    x.isDrivePowerOn()
     x.delete();
 end
